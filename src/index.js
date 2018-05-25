3f6338c3a23fa42deb8fcadcbc6ecff3e814d52f
estefaniatelis <estefaniatelis@gmail.com>
2018-05-25 16:42:26 -0500

    subiendo mis cambios

---
+++ src/index.js
@@ -0,0 +1,5 @@
+//Calling elements from the DOM
+var input = document.getElementById("in");
+var encrypt = document.getElementById("encrypt");
+var decrypt = document.getElementById("decrypt");
+var output = document.getElementById("out");
